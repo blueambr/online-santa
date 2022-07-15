a68785c3d355dfb35210615f8351ce55fb8e12ef
Vlad Gerasimovich <hotepp@pm.me>
2022-07-16 00:53:23 +0200

    feat: add createDynamicEntities

--- components/03-sections/FormEvent/index.js
+++ components/03-sections/FormEvent/index.js
@@ -1,7 +1,11 @@
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { nanoid } from "nanoid";
 import getIcon from "utils/getIcon";
 
 const FormEvent = ({ data }) => {
+  const [dynamicEntities, setDynamicEntities] = useState([]);
+
   const renderEntityFields = (entity) =>
     entity.fields.map((field) => {
       if (field.options) {
@@ -36,32 +40,76 @@ const FormEvent = ({ data }) => {
 
   const renderFields = (field) => {
     if (field.isDynamic) {
-      return field.entities.map((entity) => (
+      return (
         <>
-          <div className="flex flex-col items-center gap-4 md:flex-row md:items-start">
-            <div className="flex w-full flex-col gap-4 md:block md:columns-3">
-              {renderEntityFields(entity)}
-            </div>
-            <button
-              className="btn btn-outline btn-error btn-square relative"
-              title={field.buttonRemove}
-              type="button"
+          {dynamicEntities.map((entity) => (
+            <div
+              className="flex flex-col items-center gap-4 md:flex-row md:items-start"
+              key={entity.id}
             >
-              <FontAwesomeIcon
-                className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-                icon={getIcon("xmark")}
-                size="2xl"
-              />
-            </button>
-          </div>
-          <button className="btn btn-primary mt-8 gap-2" type="button">
+              <div className="flex w-full flex-col gap-4 md:block md:columns-3">
+                {renderEntityFields(entity)}
+              </div>
+              <button
+                className="btn btn-outline btn-error btn-square relative"
+                title={field.buttonRemove}
+                type="button"
+              >
+                <FontAwesomeIcon
+                  className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                  icon={getIcon("xmark")}
+                  size="2xl"
+                />
+              </button>
+            </div>
+          ))}
+          <button
+            className="btn btn-primary mt-8 gap-2"
+            type="button"
+            onClick={() => createDynamicEntities()}
+          >
             <FontAwesomeIcon icon={getIcon("plus")} size="xl" />
             {field.buttonAdd}
           </button>
         </>
-      ));
+      );
     }
   };
+
+  const createDynamicEntities = (isFirstRun = false) => {
+    data.fields.map((field) => {
+      if (field.isDynamic) {
+        const { entity } = field;
+
+        if (isFirstRun) {
+          setDynamicEntities([entity]);
+        } else {
+          const splitColon = JSON.stringify(entity).split(":");
+
+          const splitColonUpdated = splitColon.map((item, index) => {
+            if (item.includes('"id"')) {
+              const idItemSplitComma = splitColon[index + 1].split(",");
+
+              idItemSplitComma[0] = `"${nanoid()}"`;
+
+              splitColon[index + 1] = idItemSplitComma.join();
+            }
+
+            return item;
+          });
+
+          setDynamicEntities([
+            ...dynamicEntities,
+            JSON.parse(splitColonUpdated.join(":")),
+          ]);
+        }
+      }
+    });
+  };
+
+  useEffect(() => {
+    createDynamicEntities(true);
+  }, []);
 
   return (
     <>
